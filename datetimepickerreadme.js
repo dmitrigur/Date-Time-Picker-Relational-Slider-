5d182498569455fb1c3662113ca0a8d50ccbdb36
dmitrigur <dmitrig45@gmail.com>
2015-03-07 10:12:22 +0100

    Update datetimepickerreadme.js

--- datetimepickerreadme.js
+++ datetimepickerreadme.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-	$(this).HorecaTechDateTimePicker({
-		ResultMode:"object",
+	$('#a1').HorecaTechDateTimePicker({
 		TruncateResult:true,
 		LevelSet:["FullYear","Month"],
 		onClose:function(elm,ts,result) {
-			sort=$(elm).attr("sort"),
-			record=$(elm).attr("record");
-			$("[param='Ybeg'][sort='"+sort+"'][record='"+record+"']").val(result.FullYear);
-			$("[param='Mbeg'][sort='"+sort+"'][record='"+record+"']").val(result.Month+1);
-			$("[param='Ybeg'][sort='"+sort+"'][record='"+record+"'],[param='Mbeg'][sort='"+sort+"'][record='"+record+"']").attr("ts",ts);
-			$("[param='Ybeg'][sort='"+sort+"'][record='"+record+"'],[param='Mbeg'][sort='"+sort+"'][record='"+record+"']").trigger("change");
+			null;
 		}
 	})
 }
