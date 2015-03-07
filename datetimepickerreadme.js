c56c0c39e66541bf5c115019e1fe5231a9d7bed9
dmitrigur <dmitrig45@gmail.com>
2015-03-07 10:26:05 +0100

    Update datetimepickerreadme.js

--- datetimepickerreadme.js
+++ datetimepickerreadme.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-	$('#a1').HorecaTechDateTimePicker({
+	$("#a1").HorecaTechDateTimePicker({
+		TruncateResult:true,
+		onClose:function(elm,ts,result) {
+			$(elm).val(result);
+			$(elm).attr("ts",ts);
+		}
+	})
+	$("#a2").HorecaTechDateTimePicker({
+		ResultMode:"date",
+		TruncateResult:true,
+		LevelSet:["FullYear","Month","Date"],
+		onClose:function(elm,ts,result) {
+			$(elm).val(result);
+			$(elm).attr("ts",ts);
+		}
+	})
+	$("#a3").HorecaTechDateTimePicker({
+		ResultMode:"object",
 		TruncateResult:true,
 		LevelSet:["FullYear","Month"],
 		onClose:function(elm,ts,result) {
-			null;
+			$(elm).val(result.FullYear+" / "+result.Month);
+			$(elm).attr("ts",ts);
 		}
 	})
 }
